72eeaaf2a27488f43fe700a2d1f468cc6b5eae41
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-21 00:02:56 +0600

    Header added

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+        reiciendis saepe dolores nisi repellendus sapiente, iusto voluptas vero
+        laudantium vitae repellat sint ipsa provident dolorem nobis repudiandae
+        harum consequatur nesciunt molestiae sunt inventore iure, nemo in.
+        Dolorem quae molestias nostrum. Cupiditate, fuga ullam! Laboriosam aut
+        repudiandae esse quidem sequi sint saepe laudantium. Asperiores, quia ea
+        similique dolorem quaerat odit voluptatum, eligendi fuga ipsa
+        praesentium atque impedit odio adipisci animi et rerum sunt facere.
+        Velit exercitationem voluptatum perspiciatis doloremque quia pariatur
+        sunt officia temporibus magni autem odit possimus, facere voluptate.
+        Similique ad fuga sit qui voluptates necessitatibus obcaecati, fugit ab
+        vel!
+      </h1>
+    </div>
+  );
+};
+
+export default Blog;
