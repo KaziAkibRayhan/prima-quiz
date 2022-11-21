eb37979b970c98f8b4423c6c57e7511304f22de2
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-21 14:26:23 +0600

    quiz selection functionality added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,22 +1,48 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const Question = ({ query, total }) => {
+const Question = ({ query }) => {
   const { options, id, question, correctAnswer } = query;
+
+  console.log(correctAnswer);
+  let answer;
+
+  const handleCheckAnswer = (e) => {
+    answer = e.target.innerText;
+    if (correctAnswer === answer) {
+      toast.success("Correct Answer", { autoClose: 800 });
+    } else {
+      toast.warning("Answer is not Correct ! try again...", { autoClose: 800 });
+    }
+  };
+
   return (
     <div>
       <h3 className="text-2xl border p-4">Quiz: {question}</h3>
       <div className="flex justify-center">
         <div className="w-1/2">
-          <div className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200">
+          <div
+            onClick={(e) => handleCheckAnswer(e)}
+            className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200"
+          >
             {options[0]}
           </div>
-          <div className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200">
+          <div
+            onClick={(e) => handleCheckAnswer(e)}
+            className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200"
+          >
             {options[1]}
           </div>
-          <div className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200">
+          <div
+            onClick={(e) => handleCheckAnswer(e)}
+            className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200"
+          >
             {options[2]}
           </div>
-          <div className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200">
+          <div
+            onClick={(e) => handleCheckAnswer(e)}
+            className="bg-slate-300 m-6 p-3 rounded-md cursor-pointer hover:bg-slate-200"
+          >
             {options[3]}
           </div>
         </div>
